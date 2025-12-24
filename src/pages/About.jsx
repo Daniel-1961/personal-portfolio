8ecfa2540ce4856661d2b2c905829ee068d6e971
Daniel-1961 <danieldata36@gmail.com>
2025-12-24 22:17:53 +0300

    update to resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -35,7 +35,7 @@ export default function About (){
 
                
                     <a
-                    href=""
+                    href=" "
                     download
                     className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700 transition"
                 >
